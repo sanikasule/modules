27b158f43d71c8c05d8ffb4e3bcef3c4c981ac64
sanikasule <sulesanika25@gmail.com>
2026-03-16 16:58:08 +0530

    routing changes

--- src/pages/Handshake.tsx
+++ src/pages/Handshake.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Handshake() {
+    const navigate = useNavigate();
+
     return (
         <div className="content-center p-4">
             <h1 className="mb-5 font-mono">Welcome</h1>
-            <button className="cursor-pointer font-mono p-4">
+            <button className="cursor-pointer font-mono p-4" onClick={() => navigate('/login')}>
                 Get Started
             </button>
         </div>
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Handshake from "../pages/Handshake";
+import Login from "../pages/Login";
 
 export default function PublicRouter() {
     return(
         <Routes>
             <Route path="/" element={<Handshake />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     )
 }
